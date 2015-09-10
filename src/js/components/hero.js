b567785fdcd9b436d04a6d4294c5d99a5329d1b8
Malcolm Ahoy <malcolmahoy@gmail.com>
2015-09-10 14:56:13 -0700

    Remove classnames library and its usage
    It wasn't being used to full capacity and isn't really needed (right now at least). YAGNI.

--- src/js/components/hero.js
+++ src/js/components/hero.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import classnames from 'classnames';
 
 
 export default React.createClass({
   render: function () {
-    var classes = classnames('hero');
-
     return (
-      <div className={ classes }>
+      <div className='hero'>
         <div className='hero__mid'>
           <h1 className='hero__name'>Malcolm Ahoy</h1>
           <h2 className='hero__title'>Web Application Developer.</h2>
