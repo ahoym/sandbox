2cfd57a539893cdb91a6d7ff8eb4cef10965b397
Malcolm Ahoy <malcolmahoy@gmail.com>
2015-09-10 15:19:15 -0700

    Change hero button text again

--- src/js/components/hero.js
+++ src/js/components/hero.js
@@ -8,7 +8,7 @@ export default React.createClass({
         <div className='hero__mid'>
           <h1 className='hero__name'>Malcolm Ahoy</h1>
           <h2 className='hero__title'>Web Application Developer.</h2>
-          <button className='hero__btn'>Click Me</button>
+          <button className='hero__btn'>Get to know me</button>
         </div>
       </div>
     )
